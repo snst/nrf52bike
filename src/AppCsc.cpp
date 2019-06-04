8758c1900de7378e2943a5fcc271da12df7283fc
snst <stefan@meek.de>
2019-06-04 23:42:34 +0200

    fixed overflow bug

--- src/AppCsc.cpp
+++ src/AppCsc.cpp
@@ -9,7 +9,7 @@
 #define MAX_SPEED (999u)
 #define MAX_DISTANCE (99999999u)
 #define MAX_CADENCE (199u)
-#define MAX_TIME (((99u*3600u)+(99u*60u)+59u) *1000u)
+#define MAX_TIME (((99u * 3600u) + (99u * 60u) + 59u) * 1000u)
 
 // 211.4f
 AppCsc::AppCsc()
@@ -22,6 +22,34 @@ AppCsc::AppCsc()
 
 AppCsc::~AppCsc()
 {
+}
+
+uint16_t GetDiffUInt16(uint16_t now, uint16_t last)
+{
+    uint16_t diff;
+    if (now >= last)
+    {
+        diff = now - last;
+    }
+    else
+    {
+        diff = now + (0xFFFF - last);
+    }
+    return diff;
+}
+
+uint32_t GetDiffUInt32(uint32_t now, uint32_t last)
+{
+    uint32_t diff;
+    if (now >= last)
+    {
+        diff = now - last;
+    }
+    else
+    {
+        diff = now + (0xFFFFFFFF - last);
+    }
+    return diff;
 }
 
 bool AppCsc::ProcessData(uint32_t now_ms, const uint8_t *data, uint32_t len)
@@ -56,17 +84,20 @@ bool AppCsc::ProcessData(uint32_t now_ms, const uint8_t *data, uint32_t len)
         if (is_init_)
         {
             uint32_t delta_ms = now_ms - data_.timestamp_ms;
-            uint32_t wheel_counter_diff = msg.wheelCounter - last_msg_.wheelCounter;
-            uint32_t wheel_event_diff = msg.wheelEvent - last_msg_.wheelEvent;
+            uint32_t wheel_counter_diff = GetDiffUInt32(msg.wheelCounter, last_msg_.wheelCounter);
+            uint32_t wheel_event_diff = GetDiffUInt16(msg.wheelEvent, last_msg_.wheelEvent);
             double delta_sec = wheel_event_diff / 1024.0;
             double speed_kmh = 0.0f;
+
+            FLOW("WE %u %u\r\n", msg.wheelEvent, wheel_event_diff);
 
             bool is_riding = wheel_counter_diff > 0;
             data_.is_riding = is_riding;
 
             data_.total_wheel_rounds += wheel_counter_diff;
 
-            uint16_t speed_kmhX10 = 0;;
+            uint16_t speed_kmhX10 = 0;
+            
             if (wheel_event_diff)
             {
                 // cm/sec * 3600 / 100 / 1000
@@ -74,8 +105,8 @@ bool AppCsc::ProcessData(uint32_t now_ms, const uint8_t *data, uint32_t len)
             }
             data_.speed_kmhX10 = MIN(speed_kmhX10, MAX_SPEED);
 
-            uint32_t crank_counter_diff = msg.crankCounter - last_msg_.crankCounter;
-            uint32_t crank_event_diff = msg.crankEvent - last_msg_.crankEvent;
+            uint32_t crank_counter_diff = GetDiffUInt16(msg.crankCounter, last_msg_.crankCounter);
+            uint32_t crank_event_diff = GetDiffUInt16(msg.crankEvent, last_msg_.crankEvent);
             double crank_delta_sec = crank_event_diff / 1024.0f;
 
             uint16_t cadence = 0;
@@ -83,7 +114,8 @@ bool AppCsc::ProcessData(uint32_t now_ms, const uint8_t *data, uint32_t len)
             if (crank_delta_sec > 0.0f)
             {
                 cadence = crank_counter_diff * 60.0f / crank_delta_sec;
-                if (cadence>30) {
+                if (cadence > 30)
+                {
                     crank_event_sum_ += crank_event_diff;
                     crank_counter_sum_ += crank_counter_diff;
                     average_cadence = crank_counter_sum_ * 60.0f / ((double)crank_event_sum_ / 1024.0f);
@@ -95,7 +127,7 @@ bool AppCsc::ProcessData(uint32_t now_ms, const uint8_t *data, uint32_t len)
             uint16_t filtered_cadence = AddFilterVal(filtered_cadence_, data_.cadence);
             data_.filtered_cadence = MIN(filtered_cadence, MAX_CADENCE);
 
-/*
+            /*
             INFO("wc=%u, we=%u, dwc=%u, dwe=%u, ms=%u, s=%f, r=%u\r\n",
                  msg.wheelCounter, msg.wheelEvent, wheel_counter_diff, wheel_event_diff,
                  delta_ms, delta_sec, data_.total_wheel_rounds);
@@ -106,8 +138,8 @@ bool AppCsc::ProcessData(uint32_t now_ms, const uint8_t *data, uint32_t len)
                 trip_time_ms += delta_ms;
             }
             data_.trip_time_ms = MIN(trip_time_ms, MAX_TIME);
-            static uint8_t k=0;
-            INFO("[%i]now: %ums, diff=%ums, %f kmh, cadence=%u/min, time=%u\r\n", k++, now_ms, delta_ms, data_.speed_kmhX10 / 10.0f, (uint16_t)data_.cadence, data_.trip_time_ms / 1000);
+            static uint8_t k = 0;
+            FLOW("[%i]now: %ums, diff=%ums, %f kmh, cadence=%u/min, time=%u\r\n", k++, now_ms, delta_ms, data_.speed_kmhX10 / 10.0f, (uint16_t)data_.cadence, data_.trip_time_ms / 1000);
 
             uint32_t trip_distance_cm = (uint32_t)(data_.total_wheel_rounds * wheel_size_cm_);
             data_.trip_distance_cm = MIN(trip_distance_cm, MAX_DISTANCE);
@@ -136,7 +168,7 @@ void AppCsc::CalculateAverageSpeed()
     uint16_t filtered_speed_kmhX10 = AddFilterVal(filtered_speed_kmhX10_, data_.speed_kmhX10);
     data_.filtered_speed_kmhX10 = MIN(filtered_speed_kmhX10, MAX_SPEED);
     data_.max_speed_kmhX10 = MAX(data_.max_speed_kmhX10, data_.filtered_speed_kmhX10);
-/*
+    /*
     uint32_t sum = filtered_speed_kmhX10_[0] + data_.speed_kmhX10;
     for(size_t i=1; i<FILTER_VALUES_MAX; i++) {
         sum += filtered_speed_kmhX10_[i];
@@ -150,11 +182,12 @@ void AppCsc::CalculateAverageSpeed()
 uint16_t AppCsc::AddFilterVal(uint16_t array[], uint16_t val)
 {
     uint32_t sum = array[0] + val;
-    for(size_t i=1; i<FILTER_VALUES_MAX; i++) {
+    for (size_t i = 1; i < FILTER_VALUES_MAX; i++)
+    {
         sum += array[i];
-        array[i-1] = array[i];
+        array[i - 1] = array[i];
     }
-    array[FILTER_VALUES_MAX-1] = val;
+    array[FILTER_VALUES_MAX - 1] = val;
     uint16_t ret = sum / FILTER_VALUES_CNT;
     return ret;
 }
